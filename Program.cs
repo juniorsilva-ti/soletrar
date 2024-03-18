d2a408f6b9afcf75010b0187b2670c5c963c533e
juniorsilva-ti <mundo.com231@gmail.com>
2024-03-17 22:03:24 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(" \n");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("SOLETRANDO \n");
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Write("Digite uma palavra: ");
+string palavra = Console.ReadLine()!;
+
+var caracteres = palavra.ToCharArray();
+string palavraSoletrada = String.Join('-', caracteres).ToUpper();
+
+Console.WriteLine($"\nSoletrando \"{palavra.ToUpper()}\": {palavraSoletrada} \n");
